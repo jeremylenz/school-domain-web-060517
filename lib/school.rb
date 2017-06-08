5870b0eee422562d1380e8439558cd3dc9586e1d
jeremylenz <jeremylenz@gmail.com>
2017-06-08 14:51:57 -0400

    sort exclamation marrkkkkk

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+class School
+
+attr_reader :roster
+
+def initialize(name)
+  @name = name
+  @roster = {}
+end
+
+def add_student(student, grade)
+  if @roster[grade] == nil
+    roster[grade] = [student]
+  else
+    roster[grade] << student
+  end
+end
+
+def grade(student_year)
+roster[student_year]
+end
+
+def sort
+
+@roster.each do |grade, student_list|
+  @roster[grade].sort!
+end
+
+end
+
+end
